30e314fa3f5212fa9222758f84a20f5d0cbffe94
Daffa <daffa.ahmadrivaldi.tik19@mhsw.pnj.ac.id>
2022-01-09 21:16:00 +0700

    changes: tambahan

--- analisa-alternatif-tabel.php
+++ analisa-alternatif-tabel.php
@@ -14,46 +14,51 @@ if (isset($altkriteria)) {
 	if (isset($_POST['submit'])) {
 		$altCount = $altObj->countByFilter();
 
-		$no=1; $r = []; $nid = [];
+		$no = 1;
+		$r = [];
+		$nid = [];
 		$alt1 = $altObj->readByFilter();
-		while ($row = $alt1->fetch(PDO::FETCH_ASSOC)){
+		while ($row = $alt1->fetch(PDO::FETCH_ASSOC)) {
 			$alt2 = $altObj->readByFilter();
 			while ($roww = $alt2->fetch(PDO::FETCH_ASSOC)) {
 				$nid[$row['id_alternatif']][] = $roww['id_alternatif'];
 			}
-			$total = $altCount-$no;
-			if ($total>=1) {
+			$total = $altCount - $no;
+			if ($total >= 1) {
 				$r[$row['id_alternatif']] = $total;
 			}
 			$no++;
 		}
 
-		$ni=1;
+		$ni = 1;
 		foreach ($nid as $key => $value) {
 			array_splice($nid[$key], 0, $ni++);
 		}
-		$ne = count($nid)-1;
+		$ne = count($nid) - 1;
 		array_splice($nid, $ne, 1);
 
 		// print_r($r);
 		// print_r($nid);
 		// die();
 
-		$no=1; foreach ($r as $k => $v) {
-			 $j=0; for ($i=1; $i<=$v; $i++) {
+		$no = 1;
+		foreach ($r as $k => $v) {
+			$j = 0;
+			for ($i = 1; $i <= $v; $i++) {
 				// $rows = $altObj->readSatu($k); while ($row = $rows->fetch(PDO::FETCH_ASSOC)){
-					if ($skoObj->insert($_POST[$k.$no], $_POST['nl'.$no], $_POST[$nid[$k][$j].$no], $altkriteria)) {
-						// ...
-					} else {
-						$skoObj->update($_POST[$k.$no], $_POST['nl'.$no], $_POST[$nid[$k][$j].$no], $altkriteria);
-					}
+				if ($skoObj->insert($_POST[$k . $no], $_POST['nl' . $no], $_POST[$nid[$k][$j] . $no], $altkriteria)) {
+					// ...
+				} else {
+					$skoObj->update($_POST[$k . $no], $_POST['nl' . $no], $_POST[$nid[$k][$j] . $no], $altkriteria);
+				}
 
-					if ($skoObj->insert($_POST[$nid[$k][$j].$no], 1/$_POST['nl'.$no], $_POST[$k.$no], $altkriteria)) {
-						// ...
-					} else {
-						$skoObj->update($_POST[$nid[$k][$j].$no], 1/$_POST['nl'.$no], $_POST[$k.$no], $altkriteria);
-					}
-					$no++; $j++;
+				if ($skoObj->insert($_POST[$nid[$k][$j] . $no], 1 / $_POST['nl' . $no], $_POST[$k . $no], $altkriteria)) {
+					// ...
+				} else {
+					$skoObj->update($_POST[$nid[$k][$j] . $no], 1 / $_POST['nl' . $no], $_POST[$k . $no], $altkriteria);
+				}
+				$no++;
+				$j++;
 				// }
 			}
 		}
@@ -64,13 +69,13 @@ if (isset($altkriteria)) {
 		echo "<script>location.href='analisa-alternatif.php'</script>";
 		exit;
 	}
-	?>
+?>
 	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12">
 			<ol class="breadcrumb">
-			  <li><a href="index.php">Beranda</a></li>
-			  <li><a href="analisa-alternatif.php">Analisa Alternatif</a></li>
-			  <li class="active">Tabel Analisa Alternatif</li>
+				<li><a href="index.php">Beranda</a></li>
+				<li><a href="analisa-alternatif.php">Analisa Alternatif</a></li>
+				<li class="active">Tabel Analisa Alternatif</li>
 			</ol>
 			<div class="row">
 				<div class="col-md-6 text-left">
@@ -78,27 +83,30 @@ if (isset($altkriteria)) {
 				</div>
 				<div class="col-md-6 text-right">
 					<form method="post">
-	          <button name="hapus" class="btn btn-danger">Hapus Semua Data</button>
+						<button name="hapus" class="btn btn-danger">Hapus Semua Data</button>
 					</form>
 				</div>
 			</div>
-			<br/>
+			<br />
 			<table width="100%" class="table table-striped table-bordered">
-        <thead>
+				<thead>
 					<tr>
-						<th><?=$skoObj->kri?></th>
-						<?php $alt1a = $altObj->readByFilter(); while ($row = $alt1a->fetch(PDO::FETCH_ASSOC)): ?>
-							<th><?=$row['nama']?></th>
+						<th><?= $skoObj->kri ?></th>
+						<?php $alt1a = $altObj->readByFilter();
+						while ($row = $alt1a->fetch(PDO::FETCH_ASSOC)) : ?>
+							<th><?= $row['nama'] ?></th>
 						<?php endwhile; ?>
 					</tr>
-        </thead>
+				</thead>
 				<tbody>
-					<?php $alt2a = $altObj->readByFilter(); while ($baris = $alt2a->fetch(PDO::FETCH_ASSOC)): ?>
+					<?php $alt2a = $altObj->readByFilter();
+					while ($baris = $alt2a->fetch(PDO::FETCH_ASSOC)) : ?>
 						<tr>
-							<th class="active"><?=$baris['nama']?></th>
-							<?php $alt3a = $altObj->readByFilter(); while ($kolom = $alt3a->fetch(PDO::FETCH_ASSOC)): ?>
+							<th class="active"><?= $baris['nama'] ?></th>
+							<?php $alt3a = $altObj->readByFilter();
+							while ($kolom = $alt3a->fetch(PDO::FETCH_ASSOC)) : ?>
 								<td>
-								<?php
+									<?php
 									if ($baris['id_alternatif'] == $kolom['id_alternatif']) {
 										echo '1';
 										if (!$skoObj->insert($baris['id_alternatif'], '1', $kolom['id_alternatif'], $altkriteria)) {
@@ -108,64 +116,68 @@ if (isset($altkriteria)) {
 										$skoObj->readAll1($baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
 										echo number_format($skoObj->kp, 4, '.', ',');
 									}
-								?>
+									?>
 								</td>
 							<?php endwhile; ?>
 						</tr>
 					<?php endwhile; ?>
 				</tbody>
-        <tfoot>
-         	<tr class="info">
-						<th>Jumlah</th>
-						<?php /*$jumlahBobot=[];*/ $alt4a = $altObj->readByFilter(); while ($row = $alt4a->fetch(PDO::FETCH_ASSOC)): ?>
-						<th>
-							<?php
+				<tfoot>
+					<tr class="info">
+						<th>Jumlah Rolan</th>
+						<?php /*$jumlahBobot=[];*/ $alt4a = $altObj->readByFilter();
+						while ($row = $alt4a->fetch(PDO::FETCH_ASSOC)) : ?>
+							<th>
+								<?php
 								$skoObj->readSum1($row['id_alternatif'], $altkriteria);
 								echo number_format($skoObj->nak, 4, '.', ',');
 								if (!$skoObj->insert3($row['id_alternatif'], $altkriteria, $skoObj->nak)) {
 									$skoObj->insert5($skoObj->nak, $row['id_alternatif'], $altkriteria);
 								}
 								// $jumlahBobot[$row["id_alternatif"]] = $skoObj->nak;
-							?>
-						</th>
-					<?php endwhile;?>
-          </tr>
-        </tfoot>
-		 	</table>
+								?>
+							</th>
+						<?php endwhile; ?>
+					</tr>
+				</tfoot>
+			</table>
 
 			<table width="100%" class="table table-striped table-bordered">
-	      <thead>
-		      <tr>
-	          <th>Perbandingan</th>
-	          <?php $alt1b = $altObj->readByFilter(); while ($row = $alt1b->fetch(PDO::FETCH_ASSOC)): ?>
-		          <th><?=$row['nama']?></th>
-	          <?php endwhile; ?>
-	          <th class="success">Prioritas</th>
-		      </tr>
-	      </thead>
-	      <tbody>
-					<?php $alt2b = $altObj->readByFilter(); while ($baris = $alt2b->fetch(PDO::FETCH_ASSOC)): ?>
-		        <tr>
-		          <th class="active"><?=$baris['nama']?></th>
-		          <?php $alt3b = $altObj->readByFilter(); while ($kolom = $alt3b->fetch(PDO::FETCH_ASSOC)): ?>
-		            <td>
-		            	<?php
-		                $skoObj->readAll3($kolom['id_alternatif'], $altkriteria);
-										$jumlahBobot = $skoObj->jak;
-			            	if ($baris['id_alternatif'] == $kolom['id_alternatif']) {
-			            		$n = 1/$jumlahBobot;
-			            		$skoObj->insert2($n, $baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
-			            		echo number_format($n, 4, '.', ',');
-			            	} else {
-			            		$skoObj->readAll1($baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
-		                  $bobot = $skoObj->kp;
-			            		$n = $bobot/$jumlahBobot;
-			            		$skoObj->insert2($n, $baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
-			            		echo number_format($n, 4, '.', ',');
-			            	}
-		            	?>
-		            </td>
-		          <?php endwhile; ?>
+				<thead>
+					<tr>
+						<th>Perbandingan</th>
+						<?php $alt1b = $altObj->readByFilter();
+						while ($row = $alt1b->fetch(PDO::FETCH_ASSOC)) : ?>
+							<th><?= $row['nama'] ?></th>
+						<?php endwhile; ?>
+						<th class="success">Prioritas</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $alt2b = $altObj->readByFilter();
+					while ($baris = $alt2b->fetch(PDO::FETCH_ASSOC)) : ?>
+						<tr>
+							<th class="active"><?= $baris['nama'] ?></th>
+							<?php $alt3b = $altObj->readByFilter();
+							while ($kolom = $alt3b->fetch(PDO::FETCH_ASSOC)) : ?>
+								<td>
+									<?php
+									$skoObj->readAll3($kolom['id_alternatif'], $altkriteria);
+									$jumlahBobot = $skoObj->jak;
+									if ($baris['id_alternatif'] == $kolom['id_alternatif']) {
+										$n = 1 / $jumlahBobot;
+										$skoObj->insert2($n, $baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
+										echo number_format($n, 4, '.', ',');
+									} else {
+										$skoObj->readAll1($baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
+										$bobot = $skoObj->kp;
+										$n = $bobot / $jumlahBobot;
+										$skoObj->insert2($n, $baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
+										echo number_format($n, 4, '.', ',');
+									}
+									?>
+								</td>
+							<?php endwhile; ?>
 							<th class="success">
 								<?php
 								$skoObj->readAvg($baris['id_alternatif']);
@@ -174,41 +186,48 @@ if (isset($altkriteria)) {
 								echo number_format($prioritas, 4, '.', ',');
 								?>
 							</th>
-		        </tr>
+						</tr>
 					<?php endwhile; ?>
-	      </tbody>
-		  </table>
+				</tbody>
+			</table>
 
 			<!-- <table width="100%" class="table table-striped table-bordered">
 				<thead>
 					<tr>
 						<th>Penjumlahan</th>
-						<?php //$alt1y = $skoObj->readAll2(); while ($row = $alt1y->fetch(PDO::FETCH_ASSOC)): ?>
-							<th><?//=$row['nama']?></th>
-						<?php //endwhile; ?>
+						<?php //$alt1y = $skoObj->readAll2(); while ($row = $alt1y->fetch(PDO::FETCH_ASSOC)): 
+						?>
+							<th><? //=$row['nama']
+								?></th>
+						<?php //endwhile; 
+						?>
 						<th class="info">Jumlah</th>
 					</tr>
 				</thead>
 				<tbody>
-					<?php //$sumRow = []; $alt2y = $skoObj->readAll2(); while ($baris = $alt2y->fetch(PDO::FETCH_ASSOC)): ?>
+					<?php //$sumRow = []; $alt2y = $skoObj->readAll2(); while ($baris = $alt2y->fetch(PDO::FETCH_ASSOC)): 
+					?>
 						<tr>
-							<th class="active"><?//=$baris['nama'] ?></th>
-							<?php //$jumlah = 0; $alt3y = $skoObj->readAll2(); while ($kolom = $alt3y->fetch(PDO::FETCH_ASSOC)): ?>
+							<th class="active"><? //=$baris['nama'] 
+												?></th>
+							<?php //$jumlah = 0; $alt3y = $skoObj->readAll2(); while ($kolom = $alt3y->fetch(PDO::FETCH_ASSOC)): 
+							?>
 								<td>
 								<?php
-									// if ($baris['id_alternatif'] == $kolom['id_alternatif']) {
-									// 	$c = $prioritas * 1;
-									// 	echo number_format($c, 4, '.', ',');
-									// 	$jumlah += $c;
-									// } else {
-									// 	$skoObj->readAll1($baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
-									// 	$c = $prioritas * $skoObj->kp;
-									// 	echo number_format($c, 4, '.', ',');
-									// 	$jumlah += $c;
-									// }
-									?>
+								// if ($baris['id_alternatif'] == $kolom['id_alternatif']) {
+								// 	$c = $prioritas * 1;
+								// 	echo number_format($c, 4, '.', ',');
+								// 	$jumlah += $c;
+								// } else {
+								// 	$skoObj->readAll1($baris['id_alternatif'], $kolom['id_alternatif'], $altkriteria);
+								// 	$c = $prioritas * $skoObj->kp;
+								// 	echo number_format($c, 4, '.', ',');
+								// 	$jumlah += $c;
+								// }
+								?>
 								</td>
-							<?php //endwhile; ?>
+							<?php //endwhile; 
+							?>
 							<th class="info">
 								<?php
 								// $sumRow[$baris['id_alternatif']] = $jumlah;
@@ -216,7 +235,8 @@ if (isset($altkriteria)) {
 								?>
 							</th>
 						</tr>
-					<?php //endwhile;?>
+					<?php //endwhile;
+					?>
 				</tbody>
 			</table> -->
 
--- analisa-kriteria-tabel.php
+++ analisa-kriteria-tabel.php
@@ -6,7 +6,7 @@ include_once('includes/kriteria.inc.php');
 $bobotObj = new Bobot($db);
 $count = $bobotObj->countAll();
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 	$kriteriaObj = new Kriteria($db);
 	$kriteriaCount = $kriteriaObj->countAll();
 
@@ -18,27 +18,27 @@ if(isset($_POST['submit'])){
 			$pcs = explode("C", $roww['id_kriteria']);
 			$c = $kriteriaCount - $pcs[1];
 		}
-		if ($c>=1) {
+		if ($c >= 1) {
 			$r[$row['id_kriteria']] = $c;
 		}
 	}
 
-	$no=1;
+	$no = 1;
 	foreach ($r as $k => $v) {
-		for ($i=1; $i<=$v; $i++) {
+		for ($i = 1; $i <= $v; $i++) {
 			$pcs = explode("C", $k);
-			$nid = "C".($pcs[1]+$i);
+			$nid = "C" . ($pcs[1] + $i);
 
-			if ($bobotObj->insert($_POST[$k.$no], $_POST['nl'.$no], $_POST[$nid.$no])) {
+			if ($bobotObj->insert($_POST[$k . $no], $_POST['nl' . $no], $_POST[$nid . $no])) {
 				// ...
 			} else {
-				$bobotObj->update($_POST[$k.$no], $_POST['nl'.$no], $_POST[$nid.$no]);
+				$bobotObj->update($_POST[$k . $no], $_POST['nl' . $no], $_POST[$nid . $no]);
 			}
 
-			if ($bobotObj->insert($_POST[$nid.$no], 1/$_POST['nl'.$no], $_POST[$k.$no])) {
+			if ($bobotObj->insert($_POST[$nid . $no], 1 / $_POST['nl' . $no], $_POST[$k . $no])) {
 				// ...
 			} else {
-				$bobotObj->update($_POST[$nid.$no], 1/$_POST['nl'.$no], $_POST[$k.$no]);
+				$bobotObj->update($_POST[$nid . $no], 1 / $_POST['nl' . $no], $_POST[$k . $no]);
 			}
 			$no++;
 		}
@@ -53,9 +53,9 @@ if (isset($_POST['hapus'])) {
 <div class="row">
 	<div class="col-xs-12 col-sm-12 col-md-12">
 		<ol class="breadcrumb">
-		  <li><a href="index.php">Beranda</a></li>
-		  <li><a href="analisa-kriteria.php">Analisa Kriteria</a></li>
-		  <li class="active">Tabel Analisa Kriteria</li>
+			<li><a href="index.php">Beranda</a></li>
+			<li><a href="analisa-kriteria.php">Analisa Kriteria</a></li>
+			<li class="active">Tabel Analisa Kriteria</li>
 		</ol>
 		<div class="row">
 			<div class="col-md-6 text-left">
@@ -63,85 +63,92 @@ if (isset($_POST['hapus'])) {
 			</div>
 			<div class="col-md-6 text-right">
 				<form method="post">
-          <button name="hapus" class="btn btn-danger">Hapus Semua Data</button>
+					<button name="hapus" class="btn btn-danger">Hapus Semua Data</button>
 				</form>
 			</div>
 		</div>
-		<br/>
+		<br />
 		<table width="100%" class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th>Antar Kriteria</th>
-          <?php $bobots1 = $bobotObj->readAll2(); while ($row = $bobots1->fetch(PDO::FETCH_ASSOC)): ?>
-            <th><?=$row['nama_kriteria'] ?></th>
-          <?php endwhile; ?>
-        </tr>
-      </thead>
-      <tbody>
-				<?php $bobots2 = $bobotObj->readAll2(); while ($baris = $bobots2->fetch(PDO::FETCH_ASSOC)): ?>
-          <tr>
-            <th class="active"><?=$baris['nama_kriteria'] ?></th>
-            <?php $bobots3 = $bobotObj->readAll2(); while ($kolom = $bobots3->fetch(PDO::FETCH_ASSOC)): ?>
-              <td>
-              	<?php
+			<thead>
+				<tr>
+					<th>Antar Kriteria Rolan</th>
+					<?php $bobots1 = $bobotObj->readAll2();
+					while ($row = $bobots1->fetch(PDO::FETCH_ASSOC)) : ?>
+						<th><?= $row['nama_kriteria'] ?></th>
+					<?php endwhile; ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $bobots2 = $bobotObj->readAll2();
+				while ($baris = $bobots2->fetch(PDO::FETCH_ASSOC)) : ?>
+					<tr>
+						<th class="active"><?= $baris['nama_kriteria'] ?></th>
+						<?php $bobots3 = $bobotObj->readAll2();
+						while ($kolom = $bobots3->fetch(PDO::FETCH_ASSOC)) : ?>
+							<td>
+								<?php
 								if ($baris['id_kriteria'] == $kolom['id_kriteria']) {
-              		echo '1';
-              		if ($bobotObj->insert($baris['id_kriteria'], '1', $kolom['id_kriteria'])) {
+									echo '1';
+									if ($bobotObj->insert($baris['id_kriteria'], '1', $kolom['id_kriteria'])) {
 										// ...
 									} else {
-							  		$bobotObj->update($baris['id_kriteria'], '1', $kolom['id_kriteria']);
-							  	}
-              	} else {
-              		$bobotObj->readAll1($baris['id_kriteria'], $kolom['id_kriteria']);
-              		echo number_format($bobotObj->kp, 4, '.', ',');
-              	}
-              	?>
-              </td>
-            <?php endwhile; ?>
-          </tr>
+										$bobotObj->update($baris['id_kriteria'], '1', $kolom['id_kriteria']);
+									}
+								} else {
+									$bobotObj->readAll1($baris['id_kriteria'], $kolom['id_kriteria']);
+									echo number_format($bobotObj->kp, 4, '.', ',');
+								}
+								?>
+							</td>
+						<?php endwhile; ?>
+					</tr>
 				<?php endwhile; ?>
-      </tbody>
+			</tbody>
 			<tfoot>
 				<tr class="info">
 					<th>Jumlah</th>
-					<?php $stmt5 = $bobotObj->readAll2(); while ($row = $stmt5->fetch(PDO::FETCH_ASSOC)): ?>
+					<?php $stmt5 = $bobotObj->readAll2();
+					while ($row = $stmt5->fetch(PDO::FETCH_ASSOC)) : ?>
 						<th>
 							<?php
-								$bobotObj->readSum1($row['id_kriteria']);
-								echo number_format($bobotObj->nak, 4, '.', ',');
-								$bobotObj->insert3($bobotObj->nak, $row['id_kriteria']);
+							$bobotObj->readSum1($row['id_kriteria']);
+							echo number_format($bobotObj->nak, 4, '.', ',');
+							$bobotObj->insert3($bobotObj->nak, $row['id_kriteria']);
 							?>
 						</th>
 					<?php endwhile; ?>
 				</tr>
 			</tfoot>
-    </table>
+		</table>
 
 		<table width="100%" class="table table-striped table-bordered">
 			<thead>
 				<tr>
 					<th>Perbandingan</th>
-					<?php $bobots1x = $bobotObj->readAll2(); while ($row2x = $bobots1x->fetch(PDO::FETCH_ASSOC)): ?>
-						<th><?=$row2x['nama_kriteria'] ?></th>
+					<?php $bobots1x = $bobotObj->readAll2();
+					while ($row2x = $bobots1x->fetch(PDO::FETCH_ASSOC)) : ?>
+						<th><?= $row2x['nama_kriteria'] ?></th>
 					<?php endwhile; ?>
 					<th class="info">Jumlah</th>
 					<th class="success">Prioritas</th>
 				</tr>
 			</thead>
 			<tbody>
-				<?php $bobots2x = $bobotObj->readAll2(); while ($baris = $bobots2x->fetch(PDO::FETCH_ASSOC)): ?>
+				<?php $bobots2x = $bobotObj->readAll2();
+				while ($baris = $bobots2x->fetch(PDO::FETCH_ASSOC)) : ?>
 					<tr>
-						<th class="active"><?=$baris['nama_kriteria'] ?></th>
-						<?php $stmt4x = $bobotObj->readAll2(); while ($kolom = $stmt4x->fetch(PDO::FETCH_ASSOC)): ?>
+						<th class="active"><?= $baris['nama_kriteria'] ?></th>
+						<?php $stmt4x = $bobotObj->readAll2();
+						while ($kolom = $stmt4x->fetch(PDO::FETCH_ASSOC)) : ?>
 							<td>
-							<?php
+								<?php
 								if ($baris['id_kriteria'] == $kolom['id_kriteria']) {
-									$c = 1/$kolom['jumlah_kriteria'];
+									$c = 1 / $kolom['jumlah_kriteria'];
 									$bobotObj->insert2($c, $baris['id_kriteria'], $kolom['id_kriteria']);
 									echo number_format($c, 4, '.', ',');
 								} else {
 									$bobotObj->readAll1($baris['id_kriteria'], $kolom['id_kriteria']);
-									$c = $bobotObj->kp/$kolom['jumlah_kriteria'];
+									$c = $bobotObj->kp / $kolom['jumlah_kriteria'];
 									$bobotObj->insert2($c, $baris['id_kriteria'], $kolom['id_kriteria']);
 									echo number_format($c, 4, '.', ',');
 								}
@@ -172,19 +179,24 @@ if (isset($_POST['hapus'])) {
 			<thead>
 				<tr>
 					<th>Penjumlahan</th>
-					<?php $bobots1y = $bobotObj->readAll2(); while ($row = $bobots1y->fetch(PDO::FETCH_ASSOC)): ?>
-						<th><?=$row['nama_kriteria'] ?></th>
+					<?php $bobots1y = $bobotObj->readAll2();
+					while ($row = $bobots1y->fetch(PDO::FETCH_ASSOC)) : ?>
+						<th><?= $row['nama_kriteria'] ?></th>
 					<?php endwhile; ?>
 					<th class="info">Jumlah</th>
 				</tr>
 			</thead>
 			<tbody>
-				<?php $sumRow = []; $bobots2y = $bobotObj->readAll2(); while ($baris = $bobots2y->fetch(PDO::FETCH_ASSOC)): ?>
+				<?php $sumRow = [];
+				$bobots2y = $bobotObj->readAll2();
+				while ($baris = $bobots2y->fetch(PDO::FETCH_ASSOC)) : ?>
 					<tr>
-						<th class="active"><?=$baris['nama_kriteria'] ?></th>
-						<?php $jumlah = 0; $bobots3y = $bobotObj->readAll2(); while ($kolom = $bobots3y->fetch(PDO::FETCH_ASSOC)): ?>
+						<th class="active"><?= $baris['nama_kriteria'] ?></th>
+						<?php $jumlah = 0;
+						$bobots3y = $bobotObj->readAll2();
+						while ($kolom = $bobots3y->fetch(PDO::FETCH_ASSOC)) : ?>
 							<td>
-							<?php
+								<?php
 								if ($baris['id_kriteria'] == $kolom['id_kriteria']) {
 									$c = $kolom['bobot_kriteria'] * 1;
 									echo number_format($c, 4, '.', ',');
@@ -205,7 +217,7 @@ if (isset($_POST['hapus'])) {
 							?>
 						</th>
 					</tr>
-				<?php endwhile;?>
+				<?php endwhile; ?>
 			</tbody>
 		</table>
 
@@ -219,13 +231,15 @@ if (isset($_POST['hapus'])) {
 				</tr>
 			</thead>
 			<tbody>
-				<?php $total=0; $bobots1z = $bobotObj->readAll2(); while ($row1 = $bobots1z->fetch(PDO::FETCH_ASSOC)): ?>
+				<?php $total = 0;
+				$bobots1z = $bobotObj->readAll2();
+				while ($row1 = $bobots1z->fetch(PDO::FETCH_ASSOC)) : ?>
 					<tr>
-						<th class="active"><?=$row1["nama_kriteria"]?></th>
-						<th class="info"><?=number_format($sumRow[$row1["id_kriteria"]], 4, '.', ',')?></th>
-						<th class="success"><?=number_format($row1["bobot_kriteria"], 4, '.', ',');?></th>
+						<th class="active"><?= $row1["nama_kriteria"] ?></th>
+						<th class="info"><?= number_format($sumRow[$row1["id_kriteria"]], 4, '.', ',') ?></th>
+						<th class="success"><?= number_format($row1["bobot_kriteria"], 4, '.', ','); ?></th>
 						<?php $jumlah = $sumRow[$row1["id_kriteria"]] + $row1["bobot_kriteria"]; ?>
-						<th class="warning"><?=number_format($jumlah, 4, '.', ',');?></th>
+						<th class="warning"><?= number_format($jumlah, 4, '.', ','); ?></th>
 						<?php $total += $jumlah; ?>
 					</tr>
 				<?php endwhile; ?>
@@ -233,7 +247,8 @@ if (isset($_POST['hapus'])) {
 			<tfoot>
 				<tr class="danger">
 					<th colspan="3">Rata-rata</th>
-					<th><?php $rata = $total/$count; echo number_format($rata, 4, '.', ','); ?></th>
+					<th><?php $rata = $total / $count;
+						echo number_format($rata, 4, '.', ','); ?></th>
 				</tr>
 			</tfoot>
 		</table>
@@ -242,11 +257,11 @@ if (isset($_POST['hapus'])) {
 			<tbody>
 				<tr>
 					<th>N (kriteria)</th>
-					<td><?=$count?></td>
+					<td><?= $count ?></td>
 				</tr>
 				<tr>
 					<th>Hasil Akhir (X maks)</th>
-					<td><?=number_format($rata, 4, '.', ',');?></td>
+					<td><?= number_format($rata, 4, '.', ','); ?></td>
 				</tr>
 				<tr>
 					<th>IR</th>
@@ -254,11 +269,13 @@ if (isset($_POST['hapus'])) {
 				</tr>
 				<tr>
 					<th>CI</th>
-					<td><?php $ci = ($rata-$count)/($count-1); echo number_format($ci, 4, '.', ',');?></td>
+					<td><?php $ci = ($rata - $count) / ($count - 1);
+						echo number_format($ci, 4, '.', ','); ?></td>
 				</tr>
 				<tr>
 					<th>CR</th>
-					<td><?php $cr = $ci/$ir; echo number_format($cr, 4, '.', ',');?></td>
+					<td><?php $cr = $ci / $ir;
+						echo number_format($cr, 4, '.', ','); ?></td>
 				</tr>
 			</tbody>
 		</table>
